360ea0fcf29c1c9168aeb2733196babda99a6696
Shaun Cooper <shaun.cooper1998@gmail.com>
2023-11-16 19:39:46 +0000

    Removed log

--- home-page/home-page.component.js
+++ home-page/home-page.component.js
@@ -2,6 +2,5 @@ browserCheck();
 
 function browserCheck() {
   const supportSaveFile = 'showSaveFilePicker' in window;
-  console.log(supportSaveFile);
   if (!supportSaveFile) document.querySelector(".alert").style.display = "flex";
 }
